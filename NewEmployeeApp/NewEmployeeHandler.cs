0eb8c6c4f0534c74b5b2a0e7f111a31c43098253
Ryan CrawCour <ryancrawcour@users.noreply.github.com>
2018-01-10 15:27:50 -0500

    T to dyanmic for Data
    changed T to dynamic
    sometimes Data contains a sub-type .... like storeageDiagnostics below
    
        "url": "https://example.blob.core.windows.net/testcontainer/testfile.txt",
        "sequencer": "00000000000004420000000000028963",
        "storageDiagnostics": {
          "batchId": "b68529f3-68cd-4744-baa4-3c0498ec19f0"
        }
    
    when that happens, in your code with the T Data Json.net blows up
    changing Data to dynamic instead seems to work regardless of what is in Data, including sub-types

--- NewEmployeeApp/NewEmployeeHandler.cs
+++ NewEmployeeApp/NewEmployeeHandler.cs
@@ -18,7 +18,7 @@ namespace NewEmployeeApp
             public string EventType { get; set; }
             public string Subject { get; set; }
             public DateTime EventTime { get; set; }
-            public T Data { get; set; }
+            public dynamic Data { get; set; }
             public string Topic { get; set; }
         }
 
